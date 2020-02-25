d36d9a70b5141d4658bc9bc5fa614cac491224cd
5exyGuy <nickas951@gmail.com>
2020-02-25 15:03:26 +0200

    Added Unity Project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2.5f;
+
+    Rigidbody2D rb;
+    float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ADD JUMP AFTER COLLISIONS
+        direction = Input.GetAxis("Horizontal");
+
+        Vector2 pos = rb.position;
+        pos.x += direction * speed * Time.deltaTime;
+
+        rb.MovePosition(pos);
+    }
+}
