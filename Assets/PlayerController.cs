1f6c7fb53d118e9535d407e67612f811a78a01bf
BroofTerr <lukaitis.rytis@gmail.com>
2020-02-25 20:16:37 +0200

    Improved player control
    Sprite changes the direction depending on the way he's moving

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     public float speed = 2.5f;
 
     Rigidbody2D rb;
+    SpriteRenderer sr;
     float direction;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,7 +22,14 @@ public class PlayerController : MonoBehaviour
     {
         // ADD JUMP AFTER COLLISIONS
         direction = Input.GetAxis("Horizontal");
-
+        if (direction < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (direction > 0)
+        {
+            sr.flipX = false;
+        }
         Vector2 pos = rb.position;
         pos.x += direction * speed * Time.deltaTime;
 
